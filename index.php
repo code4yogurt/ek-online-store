b790a8b5def94f2b44dba0ccef7ddc35528667e9
chucabigas <chucabigas@gmail.com>
2016-07-30 17:00:25 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,82 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
+<title>EK Store</title>
+<body>
+<div style='width:100%'>
+  <div style='background-color:#b5dcb3; width:100%'>
+      <h1><a href='http://www.enchantedkingdom.ph'>Enchanted Kingdom</a></h1>
+  </div>
+ <div style='width:100%'>
+  <div style='background-color:#b5dcb3; width:100%' align='center'>
+      <a href='index.php'>| EK Store | </a>
+      <a href='apparel.php'>APPAREL | </a>
+      <a href='accessories.php'>ACCESSORIES | </a>
+      <a href='drinkware.php'>DRINK WARE | </a>
+      <a href='toys.php'>TOYS | </a>
+  </div>
+<div style="background-color:#aaa;height:500px;width:150px;float:left;">
+
+
+  </div>
 
 <?php
 
-require_once('navbar.php');
+session_start();
+require_once('../mysql_connect.php');
+
+
+if (isset($_SESSION['type'])) {
+        echo"<a href='myaccount.php'>My Account | </a>";
+	echo"<a href='cart.php'>Cart | </a>";
+	echo"<a href='logout.php'>Log out | </a>";
+}
+else{
+	echo"<a href='creation.php'>Create Account</a>
+	<a href='login.php'>Log In</a>";
+}
+
+
+$query="select * from products where status=1 AND prod_code IN (select prod_id from featured_items where date_end IS NULL)";
+$result=mysqli_query($dbc,$query);
+echo"<br><br><b>Featured Items</b>";
+echo "<table border='1'>";
+
+
+$counter=0;
+while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+
+$counter=$counter+1;
+
+if($counter==1){
+echo "<tr>";
+}
+
+echo"<td width=\"10%\"><div align=\"center\">
+<img src='{$row['image']}' height='100' width='100'/>
+";
 ?>
-
-
-
-
-    <div id="all">
-
-        <div id="content">
-
-            <div class="container">
-                
-            </div>
-
-            
-
-            <!-- *** HOT PRODUCT SLIDESHOW ***
- _________________________________________________________ -->
-            <div id="hot">
-
-                <div class="box">
-                    <div class="container">
-                        <div class="col-md-12">
-                            <h2>Featured Products</h2>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="container">
-                    <div class="product-slider">
-                        <?php
-                        $query='select * from products';
-                        $result=mysqli_query($dbc,$query);
-                        while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
-                        ?>
-
-                        <div class="item">
-                            <div class="product">
-                                <a href="detail.html">
-                                    <img src="<?php echo"{$row['image']}";?>" alt="" class="img-responsive">
-                                </a>
-                                <div class="text">
-                                    <h3><a href="detail.html"><?php echo "{$row['prod_name']}";?></a></h3>
-                                    <p class="price"><?php echo "{$row['prod_price']}";?></p>
-                                </div>
-                                <!-- /.text -->
-                            </div>
-                            <!-- /.product -->
-                        </div>
-                        <?php } ?>
-
-                    </div>
-                    <!-- /.product-slider -->
-                </div>
-                <!-- /.container -->
-
-                
-
-                
-
-            </div>
-            <!-- /#hot -->
-
-            <!-- *** HOT END *** -->
-
-           
-
-
-        </div>
-        <!-- /#content -->
+<form action="specificitem.php" method="GET">
 <?php
-require_once('footer.php');
+echo"<input type='submit' name='submit' value='{$row['prod_name']}' />
+</form>
+{$row['prod_price']}
+</div></td>";
+
+if($counter==4){
+echo"</tr>";
+}
+
+if($counter==4){
+$counter=0;
+}
+
+}
+echo '</table>';
+
+
+
+?>
+<?php
+if(isset($_GET['submit'])){
+ header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/specificitem.php");
+
+}
 ?>
 
 </body>
-
 </html>
