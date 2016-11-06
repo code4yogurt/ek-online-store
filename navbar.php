5880cabb0b27a53db384f75eca82cdb3d8375ef7
devlordcarlos <miguel.dignadice@outlook.com>
2016-11-07 00:45:47 +0800

    Updated navbar
    Now with cart icon showing how many items there are in the cart.

--- navbar.php
+++ navbar.php
@@ -42,28 +42,28 @@
 
     <!-- *** TOPBAR ***
     _________________________________________________________ -->
-    
-
     <div id="top">
         <div class="container">
             <div class="col-md-12" data-animate="fadeInDown">
                 <ul class="menu">
 
                     <?php
-
-                    if(isset($_SESSION['type'])){
+               
+                    require_once('connect.php');  
+                    if(isset($_SESSION['is_loggedin'])){
+                        if($_SESSION['is_loggedin'] == 0){
+                            echo "<li><a href='#' data-toggle='modal' data-target='#login-modal'>Login</a></li>";
+                            echo "<li><a href='signup.php'>Register</a></li>";
+                        }
+                        else{
                             echo "<li><a href='logout.php'>Logout</a></li>";
                             echo "<li><a href='orderstatus.php'>Check Order</a></li>";
-                    
-                    }        
-                    else{
-                     echo "<li><a href='login.php' data-toggle='modal' data-target='#login-modal'>Login</a></li>";
-                            echo "<li><a href='creation.php'>Register</a></li>";
-                       
                         }
-                    
-                    
-
+                    }
+                    else{
+                        echo "<li><a href='#' data-toggle='modal' data-target='#login-modal'>Login</a></li>";
+                        echo "<li><a href='signup.php'>Register</a></li>";
+                    }
                     ?>
 
                     <li><a href="contact.html">Contact</a></li>
@@ -71,9 +71,6 @@
                 </ul>
             </div>
         </div>
-
-
-
 
         <!-- MODAL -->
         <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
@@ -86,9 +83,7 @@
                     </div>
 
                     <?php
-
                     if(isset($_POST['modal-login'])){ 
-
                             //checks if username field is empty
                         if(empty($_POST['un-modal'])){
                             $un=FALSE;
@@ -97,7 +92,6 @@
                         else{
                             $un=$_POST['un-modal'];
                         }
-
                             //checks if password field is empty
                         if(empty($_POST['pw-modal'])){
                             $pw=FALSE;
@@ -106,7 +100,6 @@
                         else{
                             $pw=$_POST['pw-modal'];
                         }
-
                             //makes sure username and password have values before error check
                         if($un && $pw){
                             $salt=sha1(md5($pw));
@@ -114,14 +107,11 @@
                             $query="select account_id, username, type from accounts where username='$un' and password='$pwchk'";
                             $result=@mysqli_query($dbc, $query);
                             $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
-
                             if($row){
                                 $_SESSION['username']=$row['username'];
                                 $_SESSION['type']=$row['type'];
-
                                 $_SESSION['is_loggedin']=1;
                                 $_SESSION['acc_id']=$row['account_id'];
-
                                 if($row['type']=='uac'){
                                     header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/index.php");
                                 }
@@ -141,7 +131,6 @@
                             echo "<font color='red'> Please try again! </font>";
                         }
                     }
-
                     ?>
 
                     <div class="modal-body">
@@ -169,14 +158,11 @@
     </div>
 
     <?php
-
         if(isset($_POST['search'])){
-
             //makes sure search has value
             if(isset($_POST['search-input'])){
                 $_SESSION['searchstring']=$_POST['search-input'];
             }
-
             //double checks search has value
             if($_SESSION['searchstring']){
                 header("Location: searchresults.php");
@@ -193,8 +179,8 @@
             <div class="navbar-header">
 
                 <a class="navbar-brand home" href="index.php" data-animate-hover="bounce">
-                    <img src="http://enchantedkingdom.ph/wp-content/uploads/2014/10/EK-Logo2-mobile.png" alt="EK" height="70" width="250"class="hidden-xs">
-                    <img src="http://enchantedkingdom.ph/wp-content/uploads/2014/10/EK-Logo2-mobile.png" alt="EK" height="70" width="250" class="visible-xs"><span class="sr-only">Enchanted Kingdom Home</span>
+                    <img src="img/ek2.png" alt="Obaju logo" class="hidden-xs">
+                    <img src="img/ek2.png" alt="Obaju logo" class="visible-xs"><span class="sr-only">Enchanted Kingdom Home</span>
                 </a>
                 <div class="navbar-buttons">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
@@ -205,7 +191,7 @@
                         <span class="sr-only">Toggle search</span>
                         <i class="fa fa-search"></i>
                     </button>
-                    <a class="btn btn-default navbar-toggle" href="cart.php">
+                    <a class="btn btn-default navbar-toggle" href="basket.html">
                         <i class="fa fa-shopping-cart"></i>  <span class="hidden-xs">3 items in cart</span>
                     </a>
                 </div>
@@ -218,13 +204,13 @@
                     <li class="active"><a href="index.php">Home</a>
                     </li>
                     <li class="dropdown yamm-fw">
-                        <a href="apparel.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Apparel <b class="caret"></b></a>
+                        <a href="allApparel.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Apparel <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
                                 <div class="yamm-content">
                                     <div class="row">
                                         <div class="col-lg-2">
-                                            <h5><a href="apparel.php">All</a></h5>
+                                            <h5><a href="allApparel.php">All</a></h5>
                                         </div>
                                         <div class="col-lg-2">
                                             <h5><a href="shirts.php">Shirts</a></h5>
@@ -248,13 +234,13 @@
                     </li>
 
                     <li class="dropdown yamm-fw">
-                        <a href="drinkware.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">DrinkWare <b class="caret"></b></a>
+                        <a href="alDrinkware.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">DrinkWare <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
                                 <div class="yamm-content">
                                     <div class="row">
                                         <div class="col-lg-3">
-                                            <h5><a href="drinkware.php">All</a></h5>
+                                            <h5><a href="allDrinkware.php">All</a></h5>
                                         </div>
                                         <div class="col-lg-3">
                                             <h5><a href="mugs.php">Mugs</a></h5>
@@ -277,13 +263,13 @@
                     </li>
 
                     <li class="dropdown yamm-fw">
-                        <a href="toys.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Toys <b class="caret"></b></a>
+                        <a href="allToys.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Toys <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
                                 <div class="yamm-content">
                                     <div class="row">
                                         <div class="col-lg-3">
-                                            <h5><a href="toys.php">All</a></h5>
+                                            <h5><a href="allToys.php">All</a></h5>
                                         </div>
                                         <div class="col-lg-3">
                                             <h5><a href="plushies.php">Plushies</a></h5>
@@ -299,13 +285,13 @@
                     </li>
 
                     <li class="dropdown yamm-fw">
-                        <a href="accessories.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Accessories <b class="caret"></b></a>
+                        <a href="allAccessories.php" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="200">Accessories <b class="caret"></b></a>
                         <ul class="dropdown-menu">
                             <li>
                                 <div class="yamm-content">
                                     <div class="row">
                                         <div class="col-lg-2">
-                                            <h5><a href="accessories.php">All</a></h5>
+                                            <h5><a href="allAccessories.php">All</a></h5>
                                         </div>
                                         <div class="col-lg-2">
                                             <h5><a href="braceletes.php">Braceletes</a></h5>
@@ -328,11 +314,27 @@
                             <div class="navbar-buttons">
 
                                 <!--CART-->
+                                
                                 <div class="navbar-collapse collapse right" id="basket-overview">
-                                    <a href="cart.php" class="btn btn-primary navbar-btn"><i class="fa fa-shopping-cart"></i></a>
+                                    <a href="cart.php" class="btn btn-primary navbar-btn">
+
+                                        <?php
+                                        if(isset($_SESSION['is_loggedin'])){
+                                            if($_SESSION['is_loggedin']==1){
+                                                $query1="SELECT count(event_id) AS cartCount FROM cart WHERE account_id='{$_SESSION['acc_id']}' and cart_status=1 ";
+                                                $result1=mysqli_query($dbc,$query1);
+                                                while($row1=mysqli_fetch_array($result1,MYSQLI_ASSOC)){   
+                                                    echo "{$row1['cartCount']}";
+                                                    }
+                                                }else{
+                                                echo " ";
+                                            }
+                                        }
+                                        ?>
+                                        <i class="fa fa-shopping-cart"></i></a>
                                 </div>
                                 <!--/CART COLLAPSE -->
-
+                        
                                 <!--SEARCH-->
                                 <div class="navbar-collapse collapse right" id="search-not-mobile">
                                     <button type="button" class="btn navbar-btn btn-primary" data-toggle="collapse" data-target="#search">
