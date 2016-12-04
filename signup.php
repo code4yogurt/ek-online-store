5f4eb4f2db7755deff98431c3a31de9e5eac9c4c
devlordcarlos <miguel.dignadice@outlook.com>
2016-12-04 16:59:14 +0800

    Screwy signup thing

--- signup.php
+++ signup.php
@@ -1,7 +1,5 @@
 <?php
 
-<?php
-
 session_start();
 require_once('/connect.php');
 
@@ -72,67 +70,79 @@ if(isset($_POST['login'])){
 if(isset($_POST['register'])){
 
     //EMAIL
-    $em=$_POST['reg-email'];
+    if(isset($_POST['reg-email'])){
+        $em=$_POST['reg-email'];
+    }
 
     //USERNAME
-    $un=$_POST['reg-username'];
+    if(isset($_POST['reg-username'])){
+        $un=$_POST['reg-username'];
+    }
+    
+    //PW
+    if(isset($_POST['reg-pass'])){
+        $pw=$_POST['reg-pass'];
+    }
 
-    //PW & PW VERIFICATION
-    $pw=$_POST['reg-pass'];
-    $pwv=$_POST['pwverify'];
-
+    //PW VERIFICATION
+    if(isset($_POST['pwverify'])){
+        $pwv=$_POST['pwverify'];
+    }
+    
     //BIRTHDATE
-    $bd=$_POST['reg-birthday'];
-
+    if(isset($_POST['reg-birthday'])){
+        $bd=$_POST['reg-birthday'];
+    }
+    
     //CONTACT NO
-    $cn=$_POST['reg-contactno'];
+    if(isset($_POST['reg-contactno'])){
+        $cn=$_POST['reg-contactno'];
+    }
+    
+    /* VALUES - em:E-MAIL; un:USERNAME; pw:PASSWORD; pwv:PASSWORD VERIFICATION; gd:GENDER;
+    Makes sure all the fields have are not empty */
+    if($em && $un && $pw && $pwv && $cn && $bd){
 
-        /* VALUES - em:E-MAIL; un:USERNAME; pw:PASSWORD; pwv:PASSWORD VERIFICATION; gd:GENDER;
-        Makes sure all the fields have are not empty */
-        if($em && $un && $pw && $pwv && $cn && $bd){
-
-        //checks if password matches the password verification
-            if($pw!=$pwv){
-                $pwerror_msg="Passwords do not match!";
-            }
-            else{
-
-                $salt = sha1(md5($pw));
-                $encpw = md5($pw).$salt;
-
-                $signup_query = "insert into accounts (email, username, password, birthdate, contact_number, type)
-                values ('{$em}', '{$un}', '{$encpw}', '{$bd}', '{$cn}', 'uac')";
-
-                $signup_result=@mysqli_query($dbc,$signup_query);
-
-                $_SESSION['signup-msg']="New account has been added!";
-                $_SESSION['signup-flag']=1;
-                $_SESSION['is_loggedin']=1;
-
-                $login_query = "select * from accounts where username='$un' and password='$encpw'";
-                $login_result=mysqli_query($dbc,$login_query);
-                $login_row=mysqli_fetch_array($login_result, MYSQLI_ASSOC);
-
-                $_SESSION['acc_id'] = $login_row['account_id'];
-
-                header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/index.php");
-            }
+    //checks if password matches the password verification
+        if($pw!=$pwv){
+            $pwerror_msg="Passwords do not match!";
         }
         else{
-            echo "<font color=r'red'> Please try again! </font>";
+
+            $salt = sha1(md5($pw));
+            $encpw = md5($pw).$salt;
+
+            $signup_query = "insert into accounts (email, username, password, birthdate, contact_number, type)
+            values ('{$em}', '{$un}', '{$encpw}', '{$bd}', '{$cn}', 'uac')";
+
+            $signup_result=@mysqli_query($dbc,$signup_query);
+
+            $_SESSION['signup-msg']="New account has been added!";
+            $_SESSION['signup-flag']=1;
+            $_SESSION['is_loggedin']=1;
+
+            $login_query = "select * from accounts where username='$un' and password='$encpw'";
+            $login_result=mysqli_query($dbc,$login_query);
+            $login_row=mysqli_fetch_array($login_result, MYSQLI_ASSOC);
+
+            $_SESSION['username']=$login_row['username'];
+            $_SESSION['type']=$login_row['type'];
+            $_SESSION['acc_id'] = $login_row['account_id'];
+
+            header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/index.php");
         }
-    }   
+    }
+    else{
+        echo "<font color=r'red'> Please try again! </font>";
+    }
+}   
 
-    ?>
+    require_once('/navbar.php');
+?>
 
-    <?php
-        require_once('/navbar.php');
-    ?>
+<!-- *** NAVBAR END *** -->
 
-   <!-- *** NAVBAR END *** -->
-
-   <div id="all">
-
+<div id="all">
     <div id="content">
         <div class="container">
 
@@ -142,15 +152,16 @@ if(isset($_POST['register'])){
                     <li><a href="#">Home</a></li>
                     <li>New account / Sign in</li>
                 </ul>
+
             </div>
 
             <div class="col-md-6">
                 <div class="box">
                     <h1>New account</h1>
-
                     <p class="lead">Not registered yet?</p>
 
                     <hr>
+
                     <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 
                         <div class="form-group">
@@ -183,12 +194,17 @@ if(isset($_POST['register'])){
                     </form>
                 </div>
             </div>
+
             <div class="col-md-6">
                 <div class="box">
                     <h1>Login</h1>
+
                     <p class="lead">Already our customer?</p>
+
                     <hr>
+
                     <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+
                         <div class="form-group">
                             <label for="username">Username</label>
                             <input type="text" class="form-control" name="login-username">
@@ -204,10 +220,9 @@ if(isset($_POST['register'])){
                 </div>
             </div>
         </div>
-    </div>
-    <!-- /#content -->
+    </div> <!-- /#content -->
+</div> <!-- #all -->
+
 <?php
 require_once('/footer.php');
 ?>
-</body>
-</html>
